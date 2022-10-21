be61695536ebc38d2d04e38cea34f2b5dc4f5114
gamblor21 <mark.komus@gmail.com>
2022-10-20 19:15:17 -0500

    Board cleanup and second SPI

--- Boards/Adafruit_Arcada_RP_Pico.h
+++ Boards/Adafruit_Arcada_RP_Pico.h
@@ -1,45 +1,26 @@
 #if defined(ARDUINO_ARCH_RP2040)
-//#if defined(RASPBERRY_PI_PICO)
-//#warning PICO Arcada included
+#include <hardware/spi.h>
 #include <Adafruit_GC9A01A.h>
-#include <Adafruit_ST7789.h>
-
- #if defined(GLOBAL_VAR)
-  //SPIClass SPI1(&sercom3, 6, 13, 12, SPI_PAD_0_SCK_1, SERCOM_RX_PAD_2);
- #else
-  //extern SPIClass SPI1;
- #endif
-
-/*
-#define PIN_QSPI_SCK 52
-#define PIN_QSPI_CS 56
-#define PIN_QSPI_IO0 53
-#define PIN_QSPI_IO1 55
-#define PIN_QSPI_IO2 54
-#define PIN_QSPI_IO3 51
-*/
+//#include <Adafruit_ST7789.h>
 
 #define ARCADA_TFT_SPI SPI
+#define ARCADA_TFT_RP2040_SPI spi0
 #define ARCADA_TFT_CS 17  // Display CS Arduino pin number
 #define ARCADA_TFT_DC 15  // Display D/C Arduino pin number
 #define ARCADA_TFT_RST 14 // Display reset Arduino pin number
 
-//#define ARCADA_LEFTTFT_SPI SPI1
-#define ARCADA_LEFTTFT_CS  5
-#define ARCADA_LEFTTFT_DC  6
+#define ARCADA_LEFTTFT_SPI SPI1
+#define ARCADA_LEFTTFT_RP2040_SPI spi1
+#define ARCADA_LEFTTFT_CS  13
+#define ARCADA_LEFTTFT_DC  8
 #define ARCADA_LEFTTFT_RST 14
-
-//#define ARCADA_TFT_LITE 9
 
 #define ARCADA_TFT_ROTATION 2
 #define ARCADA_TFT_DEFAULTFILL 0xFFFF
 #define ARCADA_TFT_WIDTH 240
 #define ARCADA_TFT_HEIGHT 240
 
-
 #define ARCADA_AUDIO_OUT 13
-//#define ARCADA_NEOPIXEL_PIN 8
-//#define ARCADA_NEOPIXEL_NUM 1
 
 #define ARCADA_CALLBACKTIMER 4
 #define ARCADA_CALLBACKTIMER_HANDLER TC4_Handler
@@ -52,61 +33,53 @@ class Adafruit_Arcada : public Adafruit_Arcada_SPITFT {
 public:
   Adafruit_Arcada(void) { _has_accel = false; };
 
-  //Adafruit_GC9A01A *_display, *display2;
+  Adafruit_GC9A01A *_display, *display2;
+  //Adafruit_ST7789 *_display;
 
   bool variantBegin(void) { return true; }
   
   uint32_t variantReadButtons(void) { return 0; }
 
   void displayBegin(void) {
-	bool r = SPI.setRX(16);
-	bool c = SPI.setCS(17);
-    bool s = SPI.setSCK(18);
-    bool t = SPI.setTX(19);
-	Serial.printf("SPI %d %d %d %d\n", r, c, s, t);
+	SPI.setRX(16);
+	SPI.setCS(ARCADA_TFT_CS);
+    SPI.setSCK(18);
+    SPI.setTX(19);
 	
-	Adafruit_ST7789 *tft = new Adafruit_ST7789(&ARCADA_TFT_SPI, ARCADA_TFT_CS, ARCADA_TFT_DC, -1);
-    tft->init(240, 240);
-    tft->setRotation(ARCADA_TFT_ROTATION);
-    tft->fillScreen(ARCADA_TFT_DEFAULTFILL);
+	pinMode(ARCADA_TFT_RST, OUTPUT);
+    digitalWrite(ARCADA_TFT_RST, LOW);
+    delay(10);
+    digitalWrite(ARCADA_TFT_RST, HIGH);
+    delay(10);
+
     uint8_t rtna = 0x01; // Screen refresh rate control (datasheet 9.2.18, FRCTRL2)
-    tft->sendCommand(0xC6, &rtna, 1);
-    tft->setSPISpeed(50000000); // yes fast
-    display = tft;
 	
-	//pinMode(ARCADA_TFT_RST, OUTPUT);
-    //digitalWrite(ARCADA_TFT_RST, LOW);
-    //delay(10);
-    //digitalWrite(ARCADA_TFT_RST, HIGH);
-    //delay(10);
-    
-	//_display = new Adafruit_GC9A01A(
-     // &ARCADA_TFT_SPI, ARCADA_TFT_DC, ARCADA_TFT_CS, -1);
-    //_display->begin();
-    //_display->setRotation(ARCADA_TFT_ROTATION);
-    //_display->fillScreen(ARCADA_TFT_DEFAULTFILL);
-    //uint8_t rtna =
-        //0x01; // Screen refresh rate control (datasheet 9.2.18, FRCTRL2)
-    //_display->sendCommand(0xC6, &rtna, 1);
-    //_display->setSPISpeed(50000000); // yes fast
-/*
-//#if defined(ARCADA_LEFTTFT_SPI)
-    pinPeripheral(6, PIO_SERCOM_ALT); // Enable SPI1 on RX, TX
-    pinPeripheral(13, PIO_SERCOM);
-	pinPeripheral(12, PIO_SERCOM);
+	_display = new Adafruit_GC9A01A(&ARCADA_TFT_SPI, ARCADA_TFT_DC, ARCADA_TFT_CS, -1);
+    _display->begin();
 	
-    //display2 = new Adafruit_GC9A01A(&ARCADA_LEFTTFT_SPI, ARCADA_LEFTTFT_DC,
-	display2 = new Adafruit_GC9A01A(&SPI1, ARCADA_LEFTTFT_DC,
-                                    ARCADA_LEFTTFT_CS, -1);
+	//_display = new Adafruit_ST7789(&ARCADA_TFT_SPI, ARCADA_TFT_CS, ARCADA_TFT_DC, -1);
+	//_display->init(240, 240);
+	
+    _display->sendCommand(0xC6, &rtna, 1);
+    _display->setSPISpeed(50000000); // yes fast
+    _display->setRotation(ARCADA_TFT_ROTATION);
+    _display->fillScreen(ARCADA_TFT_DEFAULTFILL);
+
+    display = _display;
+
+#if defined(ARCADA_LEFTTFT_SPI)
+	SPI1.setCS(ARCADA_LEFTTFT_CS);
+    SPI1.setSCK(10);
+    SPI1.setTX(11);
+	SPI1.setRX(12);
+	
+	display2 = new Adafruit_GC9A01A(&SPI1, ARCADA_LEFTTFT_DC, ARCADA_LEFTTFT_CS, -1);
     display2->begin();
     display2->sendCommand(0xC6, &rtna, 1); // Helps contrast a little
     display2->setSPISpeed(50000000);
     display2->setRotation(ARCADA_TFT_ROTATION);
-    display2->fillScreen(0xabc1);
-//#endif
-*/
-	//display = _display;
-
+    display2->fillScreen(ARCADA_TFT_DEFAULTFILL);
+#endif
   }
 };
 
